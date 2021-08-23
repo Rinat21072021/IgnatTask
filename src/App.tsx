94847883ce01c3427cbc4ebbc94a77f5fa59b14a
unknown <Tulendey@gmail.com>
2021-08-23 13:01:31 +0300

    task #2

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import HW1 from "./p2-homeworks/h1/HW1";
+import HW2 from "./p2-homeworks/h2/HW2";
 
 function App() {
   return (
     <div className="App">
       <HW1/>
+        <HW2/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './p1-main/m1-ui/u1-app/App'
 import * as serviceWorker from './serviceWorker'
+import HW2 from "./p2-homeworks/h2/HW2";
 
 ReactDOM.render(
     <React.StrictMode>
-        <App/>
+        <HW2/>
     </React.StrictMode>,
     document.getElementById("root")
 )
